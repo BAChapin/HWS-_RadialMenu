22d631e2a010133fcb1e71af6f4e19717a2f6a69
BAChapin <brett.chapin89@gmail.com>
2020-07-12 22:12:30 -0400

    Initial Commit

---
+++ RadialMenu/RadialMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialMenuApp.swift
+//  RadialMenu
+//
+//  Created by Brett Chapin on 7/12/20.
+//
+
+import SwiftUI
+
+@main
+struct RadialMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
